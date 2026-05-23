a03b2cd98840b3db9a55bd87715ebcc17a4b0de2
pawpriceapps <pawpriceapps@gmail.com>
2026-05-23 19:58:37 -0400

    Update search.js

--- api/search.js
+++ api/search.js
@@ -13,5 +13,6 @@ export default async function handler(req, res) {
     body: JSON.stringify(req.body)
   });
   const data = await response.json();
+  console.log('Anthropic response:', JSON.stringify(data));
   res.status(200).json(data);
 }
